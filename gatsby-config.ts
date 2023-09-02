b6b0fbf7fb2af896359d9cfc138a59316c8174fc
Doug Johnson <doug@stormtree.ca>
2023-09-02 16:33:49 -0700

    Fix issues with Chakra-UI plugin loading

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,14 +11,31 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-mdx",
-    "@chakra-ui/gatsby-plugin", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {number} [portalZIndex=undefined]
+         * The z-index to apply to all portal nodes. This is useful
+         * if your app uses a lot z-index to position elements.
+         */
+        portalZIndex: undefined,
+      }
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+  ]
 };
 
 export default config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
+import { Heading, Spinner } from "@chakra-ui/react"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <h1>Welcome to my Gatsby site!</h1>
+      <Heading>Welcome to my Gatsby site!</Heading>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <Spinner />
     </main>
   )
 }
