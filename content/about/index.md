5212585c9be09e54a7eb7b040fb99b44c8db9511
Doug Johnson <doug@stormtree.ca>
2023-09-07 12:34:33 -0700

    Configure initial Tina schema and add test content

---
+++ content/about/index.md
@@ -0,0 +1,9 @@
+---
+title_long: Resilient Urban Systems & Habitat
+title_short: '[RUSH]'
+---
+
+The RUSH (Resilient Urban Systems & Habitat) Initiative is exploring this work on the unceded and unsurrendered territories of the lək̓ʷəŋən and SENĆOŦEN speaking peoples. Maps have a long history of erasure of Indigenous cultures and territories. Our goal is to promote tools that support the healing of ecosystems and communities as a way to practice reconciliation, gratitude, and respect.
+By showcasing the research, creativity, and innovation happening in this region on an interactive landing page, we hope to provide a shared language on the vulnerabilities and opportunities for long-term health and climate action.
+We want to change the critical questions that get asked in planning meetings.  How is a development proposal an opportunity to increase resilience to climate-related events? How can we support neighbourhoods to adapt and thrive in changing conditions? If we extend and connect the ecosystem features across the urban landscape, what are all the benefits people would feel? How can we work with Nature to create a quality of life for all?
+This is a tool for people to connect habitats, restore watersheds, feel a sense of belonging, and start conversations that lead to a brighter future.  Please use the feedback button to let us know what you think and any suggestions you have.
--- tina/config.ts
+++ tina/config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "tinacms";
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
+const uiListMinMaxOpts = {
+  min: 3,
+  max: 6,
+}
+
 export default defineConfig({
   client: { skip: true },
   branch,
@@ -22,24 +27,115 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        label: "Questions",
+        name: "question",
+        path: "content/questions",
         fields: [
           {
-            type: "string",
-            name: "title",
             label: "Title",
+            name: "title",
+            type: "string",
             isTitle: true,
             required: true,
           },
           {
+            label: "Subtitle",
+            name: "question",
+            type: "string",
+            required: true,
+          },
+          {
+            label: "Section One",
+            name: "section_one",
+            type: "object",
+            fields: [
+              {
+                label: "Positive Heading",
+                name: "positive_title",
+                type: "string",
+              },
+              {
+                label: "Positive Points",
+                name: "positive_points",
+                type: "string",
+                list: true,
+                ui: uiListMinMaxOpts,
+              },
+              {
+                label: "Negative Heading",
+                name: "negative_title",
+                type: "string",
+              },
+              {
+                label: "Negative Points",
+                name: "negative_points",
+                type: "string",
+                list: true,
+                ui: uiListMinMaxOpts,
+              }
+            ]
+          },
+          {
+            label: "Section Two: Make Your Move",
+            name: "section_two",
+            type: "object",
+            fields: [
+              {
+                label: "Banner Action",
+                name: "title",
+                type: "string",
+              },
+              {
+                label: "Bullet Points",
+                name: "positive_points",
+                type: "string",
+                list: true,
+                ui: uiListMinMaxOpts,
+              },
+            ]
+          },
+          {
+            label: "Rabbit Hole",
+            name: "rabbit_hole",
             type: "rich-text",
-            name: "body",
-            label: "Body",
             isBody: true,
           },
         ],
+      },
+      {
+        label: "About Page",
+        name: "about",
+        path: "content/about",
+        match: {
+          include: "index",
+        },
+        fields: [
+          {
+            label: "Long Site Title",
+            name: "title_long",
+            type: "string",
+            required: true
+          },
+          {
+            label: "Short Site Title",
+            name: "title_short",
+            type: "string",
+            required: true
+          },
+          {
+            label: "About Text",
+            name: "body",
+            type: "rich-text",
+            required: true,
+            isBody: true,
+          },
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          }
+        }
       },
     ],
   },
